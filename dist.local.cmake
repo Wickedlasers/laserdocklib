926916147743303a5030e4d0a73593f0da5d1d26
Sergey Gavrushkin <ncuxer@gmail.com>
2016-11-05 22:07:28 +0300

    Fix bool BUILD_CMAKE

--- dist.local.cmake
+++ dist.local.cmake
@@ -1,11 +1,11 @@
 # Uncomment this line to build Android version
 # You should manually remove build dir and reopen CMake project from scratch after change this value
-set(BUILD_ANDROID true)
+set(BUILD_ANDROID OFF)
 
 # Print commands
 set(CMAKE_VERBOSE_MAKEFILE ON)
 
 # Setup Android environment
-if(DEFINED BUILD_ANDROID)
+if(BUILD_ANDROID)
     include(local.android.cmake)
 endif()

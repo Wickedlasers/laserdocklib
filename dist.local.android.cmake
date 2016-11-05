89fdbe02658dfdcb2295edb881b7de8bd9b7e81e
Sergey Gavrushkin <ncuxer@gmail.com>
2016-11-05 21:56:00 +0300

    Move all configurable properties to local*.cmake files

--- dist.local.android.cmake
+++ dist.local.android.cmake
@@ -14,12 +14,8 @@ if(ANDROID_ABI STREQUAL "armeabi-v7a")
     # Qt
     set(QTDIR "/Users/ncuxer/Dev/Qt/5.7/android_armv7")
     set(CMAKE_PREFIX_PATH "${QTDIR}/lib/cmake")
-    # Toolchain prefix: do not edit
-    set(ANDROID_TOOLCHAIN_PREFIX "arm-linux-androideabi")
 elseif(ANDROID_ABI STREQUAL "x86")
     # Qt
     set(QTDIR "/Users/ncuxer/Dev/Qt/5.7/android_x86")
     set(CMAKE_PREFIX_PATH "${QTDIR}/lib/cmake")
-    # Toolchain prefix: do not edit
-    set(ANDROID_TOOLCHAIN_PREFIX "x86")
 endif()

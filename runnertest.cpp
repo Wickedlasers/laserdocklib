c29b9fd294fed28deadebae3d2b37e3024f2f8d5
Guoping Huang <pilypala@gmail.com>
2018-07-23 04:07:08 +0800

    fixed build issue with runnertest

--- runnertest.cpp
+++ runnertest.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "LaserdockDevice.h"
+#include "LaserdockDeviceManager.h"
 
 using namespace std;
 
@@ -19,33 +20,36 @@ void print_uint32(string name, LaserdockDevice *d, ReadMethodPtr method){
 }
 
 int main() {
-    LaserdockDevice d;
-    print_uint32("Firmware major version", &d, &LaserdockDevice::version_major_number);
-    print_uint32("Firmware minor version", &d, &LaserdockDevice::version_minor_number);
-    d.set_dac_rate(30000);
-    print_uint32("Current Dac Rate", &d, &LaserdockDevice::dac_rate);
+
+    LaserdockDeviceManager &lddmanager = LaserdockDeviceManager::getInstance();
+    LaserdockDevice * d =  lddmanager.get_next_available_device();
+
+    print_uint32("Firmware major version", d, &LaserdockDevice::version_major_number);
+    print_uint32("Firmware minor version", d, &LaserdockDevice::version_minor_number);
+    d->set_dac_rate(30000);
+    print_uint32("Current Dac Rate", d, &LaserdockDevice::dac_rate);
 
     bool enabled = false ;
 
-    if(!d.enable_output()){
+    if(!d->enable_output()){
         cout << "Failed enabling output state" << endl;
     }
 
-    if(!d.get_output(&enabled)){
+    if(!d->get_output(&enabled)){
         cout << "Failed reading output state" << endl;
     } else
     {
         cout << "Output Enabled/Disabled: " << enabled << endl;
     }
 
-    d.runner_mode_enable(1);
-    d.runner_mode_enable(0);
-    d.runner_mode_run(1);
-    d.runner_mode_run(0);
+    d->runner_mode_enable(1);
+    d->runner_mode_enable(0);
+    d->runner_mode_run(1);
+    d->runner_mode_run(0);
 
 
     LaserdockSample * samples = (LaserdockSample*) calloc(sizeof(LaserdockSample), 7);
     memset(samples, 0xFF, sizeof(LaserdockSample) * 7);
-    d.runner_mode_load(samples, 0, 7);
+    d->runner_mode_load(samples, 0, 7);
     return 0;
 }
